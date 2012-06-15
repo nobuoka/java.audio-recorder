0a01e8ce8cfe189ed99c28034715022dc0230413
nobuoka <nobuoka@vividcode.info>
2012-06-16 06:54:29 +0900

    不要なメソッドを削除

--- src/info/vividcode/audio/AudioRecorderSimple.java
+++ src/info/vividcode/audio/AudioRecorderSimple.java
@@ -4,7 +4,6 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +16,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -257,125 +255,6 @@ public class AudioRecorderSimple extends Thread {
             }
         }
         worker.toBeEnd();
-    }
-
-    private static void test() {
-        try {
-            String fileName = "test";
-            Calendar calendar = Calendar.getInstance( TimeZone.getDefault() );
-            String time = String.format( "%04d-%02d-%02d_%02d%02d%02d",
-                    calendar.get( Calendar.YEAR ),
-                    calendar.get( Calendar.MONTH ) + 1,
-                    calendar.get( Calendar.DAY_OF_MONTH ),
-                    calendar.get( Calendar.HOUR ),
-                    calendar.get( Calendar.MINUTE ),
-                    calendar.get( Calendar.SECOND ) );
-            fileName += "_" + /*time +*/ ".wav";
-            File file = new File( fileName );
-            BufferedOutputStream bos;
-            bos = new BufferedOutputStream( new FileOutputStream( file ) );
-            // ヘッダーの定義
-            byte[] header = {
-                    0x52, 0x49, 0x46, 0x46, // "RIFF
-                    0,0,0,0, // ファイルサイズ [byte] - 8. あとから記述
-                    0x57, 0x41, 0x56, 0x45, // "WAVE"
-                    // ここから fmt チャンク
-                    0x66, 0x6D, 0x74, 0x20, // "fmt "
-                    0x10, 0x00, 0x00, 0x00, // fmt チャンクのバイト数
-                    0x01, 0x00, // フォーマット ID. リニア PCM
-                    0x02, 0x00, // チャンネル数. 2
-                    0x44,  -84, 0x00, 0x00, // サンプリングレート. 44.1 kHz
-                    0x10, (byte)0xB1, 0x02, 0x00, // データ速度 Byte per sec. 176,400
-                    0x04, 0x00, // ブロックサイズ
-                    0x10, 0x00, // サンプルあたりのビット数 (16 bit)
-                    // ここから data チャンク
-                    0x64, 0x61, 0x74, 0x61, // "data"
-                    0,0,0,0 // 波形データのバイト数
-                };
-            // ヘッダーの出力
-            bos.write( header );
-            // 波形データの出力
-            long dataSize = 0; // 波形データのバイト数
-System.out.println( "here1" );
-            byte[] buf = new byte[4096];
-            for ( int t = 0; t < 441000; ) {
-                for ( int j = 0; j < buf.length; ++ t, j += 4 ) {
-                    double time_s = (double)t / 44100;
-                    double freq;
-                    if ( time_s < 1 ) {
-                        freq = 110.0;// + Math.sin( 2 * Math.PI * 20 * t / 44100 );
-                    } else if ( time_s < 2 ) {
-                        freq = 220.0;
-                    } else {
-                        freq = 440.0;
-                    }
-                    double rawVal = Math.sin( 2 * Math.PI * freq /* [Hz] */ * t /* [t_unit] */ / 44100 /* [t_unit/s] */ );
-                    // rawVal は -1.0 .. 1.0 の値
-                    short val = (short)( ( rawVal ) * 0x6FFF );
-                    buf[j+0] = (byte)( val & 0xFF );
-                    buf[j+2] = (byte)( val & 0xFF );
-                    buf[j+1] = (byte)( ( val >> 8 ) & 0xFF );
-                    buf[j+3] = (byte)( ( val >> 8 ) & 0xFF );
-                    dataSize += 4;
-                }
-                bos.write( buf, 0, buf.length );
-            }
-System.out.println( "here2" );
-
-            // ファイルサイズの書き込み
-            RandomAccessFile raf = new RandomAccessFile( file, "rw" );
-            byte[] dataSizeBytes = new byte[4];
-            // 40 のところに dataSize
-            raf.seek(40);
-            for ( int i = 0; i < 4; ++ i ) // int -> bytes (little endian)
-                dataSizeBytes[i] = (byte)( (dataSize>>(i*8)) & 0xFF );
-            raf.write(dataSizeBytes);
-            // 4 のところに dataSize + 36
-            raf.seek(4);
-            dataSize += 36;
-            for ( int i = 0; i < 4; ++ i ) // int -> bytes (little endian)
-                dataSizeBytes[i] = (byte)( (dataSize>>(i*8)) & 0xFF );
-            raf.write(dataSizeBytes);
-            raf.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    public static void test2() {
-        try{
-           byte[] wave_data= new byte[44100*4];
-           double L1      = 44100.0/440.0;
-           double L2      = 44100.0/455.0;
-           for ( int t = 0, i = 0; i < wave_data.length; ++ t, i += 4 ) {
-               byte[] buf = wave_data;
-               double freq = 220;
-               double rawVal = Math.sin( 2 * Math.PI * freq /* [Hz] */ * t /* [t_unit] */ / 44100 /* [t_unit/s] */ );
-               // rawVal は -1.0 .. 1.0 の値
-               short val = (short)( ( rawVal * 0x7FFF ) );
-               buf[i+0] = (byte)( val & 0xFF );
-               buf[i+2] = (byte)( val & 0xFF );
-               buf[i+1] = (byte)( ( val >> 8 ) & 0xFF );
-               buf[i+3] = (byte)( ( val >> 8 ) & 0xFF );
-               //wave_data[i] = (byte)(55*Math.sin((i/L1)*Math.PI*2));
-               //wave_data[i]+= (byte)(55*Math.sin((i/L2)*Math.PI*2));
-           }
-
-           AudioFormat format = new AudioFormat( 44100, 16, 2, true, false );
-           AudioInputStream ais = new AudioInputStream(
-                      new ByteArrayInputStream(wave_data)
-                     ,format
-                     ,wave_data.length / 4 );
-           AudioSystem.write(
-                      ais
-                     ,AudioFileFormat.Type.WAVE
-                     ,new File("test07.wav"));
-           }
-        catch(Exception e){e.printStackTrace(System.err);}
     }
 
     private TargetDataLine getTargetDataLine() {
